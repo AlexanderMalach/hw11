47c8c8f048fd193b5e557bf89200f0e3735bcf91
79096 <a.malakhinsky@gmail.com>
2024-02-26 00:24:14 +0300

    create title and docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def to_uppercase(input_str):
-    """receiving strings and outputting words with capital letters"""
+    """ remake it in all capital letters"""
     return input_str.upper()
+
+
+def to_title(input_str):
+    """ receiving strings and outputting words with capital letters"""
+    return input_str.title()
